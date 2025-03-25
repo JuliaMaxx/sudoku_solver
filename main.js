aacc07cbfa9b10f13c38312cee48c14613a5f9bb
Julia Max <jjuliamaxxx@gmail.com>
2025-03-25 20:50:38 +0100

    feat: add input validation

--- main.js
+++ main.js
@@ -9,6 +9,14 @@ for(let row = 0; row < gridSize; row++) {
     for(let col = 0; col < gridSize; col++) {
         const cell = document.createElement("input");
         cell.classList.add("sudoku-cell");
+        cell.type = "text"; 
+        cell.maxLength = 1; 
+        cell.inputMode = "numeric"; 
+        cell.pattern = "[1-9]"; 
+        
+        cell.addEventListener("input", function () {
+            this.value = this.value.replace(/[^1-9]/g, ''); // Remove anything that is not 1-9
+        });
         
         if (col % subgridSize === subgridSize - 1) {
             cell.style.marginRight = "4px";

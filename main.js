30c19584a79e2cbdc65caaea26735ac210be3f48
Julia Max <jjuliamaxxx@gmail.com>
2025-03-25 20:21:57 +0100

    style: style the grid cells

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const grid = document.getElementById("sudoku-grid");
 const gridSize = 9;
 const subgridSize = Math.sqrt(gridSize);
 
-grid.style.gridTemplateColumns = `repeat(${gridSize}, 50px)`;
-grid.style.gridTemplateRows = `repeat(${gridSize}, 50px)`;
+grid.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
+grid.style.gridTemplateRows = `repeat(${gridSize}, auto)`;
 
 for(let row = 0; row < gridSize; row++) {
     for(let col = 0; col < gridSize; col++) {
@@ -11,16 +11,16 @@ for(let row = 0; row < gridSize; row++) {
         cell.classList.add("sudoku-cell");
         
         if (col % subgridSize === subgridSize - 1) {
-            cell.style.borderRight = "4px solid black";
+            cell.style.marginRight = "4px";
         }
         if (row % subgridSize === subgridSize - 1) {
-            cell.style.borderBottom = "4px solid black";
+            cell.style.marginBottom = "4px";
         }
         if (col % subgridSize === 0) {
-            cell.style.borderLeft = "4px solid black";
+            cell.style.marginLeft = "4px";
         }
         if (row % subgridSize === 0) {
-            cell.style.borderTop = "4px solid black";
+            cell.style.marginTop = "4px";
         }
         grid.appendChild(cell);
     }

2a0dbb6c7de05a447e07f6f9448f4f952c0dce6b
Julia Max <jjuliamaxxx@gmail.com>
2025-03-25 19:28:58 +0100

    feat: make basic grid layout

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Works")
+const grid = document.getElementById("sudoku-grid");
+const gridSize = 9;
+const subgridSize = Math.sqrt(gridSize);
+
+grid.style.gridTemplateColumns = `repeat(${gridSize}, 50px)`;
+grid.style.gridTemplateRows = `repeat(${gridSize}, 50px)`;
+
+for(let row = 0; row < gridSize; row++) {
+    for(let col = 0; col < gridSize; col++) {
+        const cell = document.createElement("div");
+        cell.classList.add("sudoku-cell");
+
+        grid.appendChild(cell);
+    }
+}

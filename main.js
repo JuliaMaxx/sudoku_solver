891c8a091a0648f459bd4622935fe4f0337efadf
Julia Max <jjuliamaxxx@gmail.com>
2025-03-25 19:54:30 +0100

    feat: distinguish subgrids

--- main.js
+++ main.js
@@ -7,9 +7,21 @@ grid.style.gridTemplateRows = `repeat(${gridSize}, 50px)`;
 
 for(let row = 0; row < gridSize; row++) {
     for(let col = 0; col < gridSize; col++) {
-        const cell = document.createElement("div");
+        const cell = document.createElement("input");
         cell.classList.add("sudoku-cell");
-
+        
+        if (col % subgridSize === subgridSize - 1) {
+            cell.style.borderRight = "4px solid black";
+        }
+        if (row % subgridSize === subgridSize - 1) {
+            cell.style.borderBottom = "4px solid black";
+        }
+        if (col % subgridSize === 0) {
+            cell.style.borderLeft = "4px solid black";
+        }
+        if (row % subgridSize === 0) {
+            cell.style.borderTop = "4px solid black";
+        }
         grid.appendChild(cell);
     }
 }
